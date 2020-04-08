37bb50e23a13ea954dc6344f79736e20f48f2282
adept-logger-6002 <jbaxter6@radford.edu>
2020-04-08 05:20:26 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,14 @@
-# Write your solution here
+
+# first get your current Time
+current_time = Time.now
+
+# next change current time to integer
+current_time = current_time.to_i
+
+#then set conditions
+
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
